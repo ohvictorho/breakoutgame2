00460900b8ed575be6776534a7753716c74aa095
Victor Ho <victor.sdho@gmail.com>
2017-08-03 18:00:24 -0700

    Initital Commit

---
+++ Breakout/Assets/scripts/Bounce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	public float ballInitialVelocity = 600f;
+
+	private Rigidbody rb;
+	private bool ballInPlay;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+				}
+		
+	}
+}
